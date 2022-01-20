6a4191312ed33de6422eea4330966ffd224f13d8
Yubin Lee <lybin10@naver.com>
2022-01-20 10:51:34 +0900

    Initial commit

--- app/src/main/java/com/example/everyteamproject/MainActivity.kt
+++ app/src/main/java/com/example/everyteamproject/MainActivity.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toolbar
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 /* <solid android:color="#6D9773" /> // 배경색 */
@@ -19,6 +16,10 @@ class MainActivity : AppCompatActivity() {
     lateinit var backButton: Button
     lateinit var addView: ImageView
     lateinit var explainText: TextView
+    lateinit var calendarView: CalendarView
+
+    var fname: String = ""
+    var str: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,26 +29,49 @@ class MainActivity : AppCompatActivity() {
         backButton = findViewById(R.id.backButton)
         addView = findViewById(R.id.addView)
         explainText = findViewById(R.id.explainText)
+        calendarView = findViewById(R.id.calendarView)
 
         /*
         addView.bringToFront()
         explainText.bringToFront()
         */
 
+        /*
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+// 달력 날짜가 선택되면
+            diaryTextView.visibility = View.VISIBLE // 해당 날짜가 뜨는 textView가 Visible
+            save_Btn.visibility = View.VISIBLE // 저장 버튼이 Visible
+            contextEditText.visibility = View.VISIBLE // EditText가 Visible
+            textView2.visibility = View.INVISIBLE // 저장된 일기 textView가 Invisible
+            cha_Btn.visibility = View.INVISIBLE // 수정 Button이 Invisible
+            del_Btn.visibility = View.INVISIBLE // 삭제 Button이 Invisible
+
+            diaryTextView.text = String.format("%d / %d / %d", year, month + 1, dayOfMonth)
+// 날짜를 보여주는 텍스트에 해당 날짜를 넣는다.
+            contextEditText.setText("") // EditText에 공백값 넣기
+
+            checkedDay(year, month, dayOfMonth) // checkedDay 메소드 호출
+
+
+        }
+        */
+
     }
+
 
     // 하단 소프트키 없애기 (몰입모드)
     override fun onWindowFocusChanged(hasFocus: Boolean) {
         super.onWindowFocusChanged(hasFocus)
         if (hasFocus) {
-            window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            window.decorView.systemUiVisibility = (
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                     or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                     or View.SYSTEM_UI_FLAG_FULLSCREEN
                     or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
         }
     }
+
 
     // 메뉴바 -> 몰입모드 실행 시 필요 X
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
