2b03c3efddc9275e547f98778d74a45733bb0e87
chaeeun-Han <becky2525@naver.com>
2022-01-24 08:38:11 +0900

    db

---
+++ app/src/main/java/com/example/everyteamproject/Project_db.kt
@@ -0,0 +1,6 @@
+package com.example.everyteamproject
+// 팀원명:역할 어떻게 받아올지 수정
+// 마감일과 마감시간 data type 수정
+class Project_db (var ProjectName:String="", var Role:String="", var Deadline:String="", var ClosingTime:String=""){
+    var id:Int = 0
+}
