06a0be954e906867b18ce0e7e4f8a90b84b52fe0
HayeongChoi28 <82139017+HayeongChoi28@users.noreply.github.com>
2022-02-02 07:09:15 +0900

    DB 일부 작업 + xml 수정 + 버튼 색상 변경

--- app/src/main/java/com/example/everyteamproject/Project_db.kt
+++ app/src/main/java/com/example/everyteamproject/Project_db.kt
@@ -1,6 +1,23 @@
 package com.example.everyteamproject
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
 // 팀원명:역할 어떻게 받아올지 수정
 // 마감일과 마감시간 data type 수정
-class Project_db (var ProjectName:String="", var Role:String="", var Deadline:String="", var ClosingTime:String=""){
-    var id:Int = 0
+
+class Project_db(
+    context: Context?,
+    name: String?,
+    factory: SQLiteDatabase.CursorFactory?,
+    version: Int
+): SQLiteOpenHelper(context, name, factory, version) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        db!!.execSQL("CREATE TABLE projects(ProjectName text, Role text, DeadLine text, ClosingTime text)")
+    }
+
+    //갱신
+    override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
+    }
 }
--- app/src/main/java/com/example/everyteamproject/Registration.kt
+++ app/src/main/java/com/example/everyteamproject/Registration.kt
@@ -1,49 +1,57 @@
 package com.example.everyteamproject
 
-import CustomDialog
 import android.app.DatePickerDialog
 import android.app.TimePickerDialog
+import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_registration.*
 import java.text.SimpleDateFormat
 import java.util.*
 
+
 class Registration : AppCompatActivity() {
-    lateinit var R_Subject: EditText
-    lateinit var R_ClosingDateBtn: Button
-    lateinit var R_ClosingTime:TextView
-    lateinit var R_ClosingDate:TextView
-    lateinit var R_EditBtn:Button
-//    lateinit var R_RoleBtn:Button
-//    lateinit var R_Role:TextView
+    var dateString = ""
+    var timeString = ""
+    lateinit var Project_db: Project_db
+    lateinit var sqlitedb: SQLiteDatabase
+    lateinit var R_DeadlineBtn: Button
+    lateinit var R_Deadline: TextView
+    lateinit var R_ClosingTime: TextView
+    lateinit var R_RoleBtn:Button
+    lateinit var R_Role:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
-        R_Subject = findViewById(R.id.R_Subject)
-        R_ClosingDateBtn = findViewById(R.id.R_ClosingDateBtn)
-        R_ClosingTime = findViewById(R.id.R_ClosingTime)
-        R_ClosingDate = findViewById(R.id.R_ClosingDate)
-        R_EditBtn = findViewById(R.id.R_EditBtn)
-//        R_RoleBtn = findViewById(R.id.R_RoleBtn)
-//        R_Role = findViewById(R.id.R_Role)
 
-        R_ClosingDateBtn.setOnClickListener{
+        Project_db = Project_db(this, "MyDB", null, 1)
+        R_DeadlineBtn = findViewById(R.id.R_DeadlineBtn)
+        R_Deadline = findViewById(R.id.R_Deadline)
+        R_ClosingTime = findViewById(R.id.R_ClosingTime)
+        R_RoleBtn = findViewById(R.id.R_RoleBtn)
+        R_Role = findViewById(R.id.R_Role)
+
+        R_DeadlineBtn.setOnClickListener {
             val cal = Calendar.getInstance()
+
             val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
                 cal.set(year, month, dayOfMonth)
                 val Date = cal.time
                 val Simpledateformat = SimpleDateFormat("EEEE", Locale.getDefault())
                 val DayName: String = Simpledateformat.format(Date)
-                R_ClosingDate.text = "${month+1}.${dayOfMonth}($DayName)"
+                dateString = "${month+1}.${dayOfMonth}($DayName)"
+                R_Deadline.text = dateString
             }
             val dpd = DatePickerDialog(this, dateSetListener, cal.get(Calendar.YEAR),cal.get(Calendar.MONTH),cal.get(Calendar.DAY_OF_MONTH))
 
-            //           최소 날짜를 현재 시각 이후로
+//           최소 날짜를 현재 시각 이후로
             dpd.datePicker.minDate = System.currentTimeMillis() - 1000;
             dpd.show()
         }
@@ -51,21 +59,38 @@ class Registration : AppCompatActivity() {
         R_ClosingTime.setOnClickListener{
             val cal = Calendar.getInstance()
             var timeSetListener = TimePickerDialog.OnTimeSetListener { view, hourOfDay, minute ->
-                R_ClosingTime.text = "${hourOfDay}"+":"+"${minute}"
-
+                timeString = "${hourOfDay}"+":"+"${minute}"
+                R_ClosingTime.text = timeString
             }
             TimePickerDialog(this, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), false).show()
         }
 
         //RoleDialog
-        R_RoleBtn.setOnClickListener{
-            val dialog = CustomDialog(this)
+        R_RoleBtn.setOnClickListener {
+            val dialog = RoleDialog(this)
             dialog.showDialog()
-            dialog.setOnClickListener(object:CustomDialog.OnDialogClickListener{
+            dialog.setOnClickListener(object : RoleDialog.OnDialogClickListener {
                 override fun onClicked(name: String) {
                     R_Role.text = name
                 }
             })
+        }
+
+        //DB
+        R_EditBtn.setOnClickListener {
+            var str_projectname: String = R_ProjectName.text.toString()
+            var str_role: String = R_Role.text.toString()
+            var str_deadline: String = R_Deadline.text.toString()
+            var str_closingtime: String = R_ClosingTime.text.toString()
+
+            sqlitedb=Project_db.writableDatabase
+            sqlitedb.execSQL("INSERT INTO projects VALUES ('"+str_projectname+"', " +
+                    "'"+str_role+"', '"+str_deadline+"', '"+str_closingtime+"')")
+            sqlitedb.close()
+
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("intent_name", str_projectname)
+            startActivity(intent)
         }
     }
 }
